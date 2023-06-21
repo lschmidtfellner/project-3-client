470d00ba0e8027c98498d9a28130a57c4ebf7958
granth2023 <grantharris38@gmail.com>
2023-06-21 10:43:56 -0400

    axios call to heroku

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import Signin from './pages/Signin';
 import Signup from './pages/Signup';
 import { AuthContext } from './context/AuthContextComponent';
@@ -7,6 +7,10 @@ import { AuthContext } from './context/AuthContextComponent';
 function App() {
   const { isLoggedIn, setIsLoggedIn } = useContext(AuthContext)
 
+  useEffect(() => {
+    console.log(isLoggedIn);
+  }, [isLoggedIn]);
+  
   return (
     <>
       <Routes>
