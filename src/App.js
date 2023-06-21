632ef6925c56f4170260366c2c5c35d3f0464aef
granth2023 <grantharris38@gmail.com>
2023-06-21 09:05:03 -0400

    front end auth

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { useContext } from 'react';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
+import { AuthContext } from './context/AuthContextComponent';
 
 function App() {
+  const { isLoggedIn, setIsLoggedIn } = useContext(AuthContext)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Navigate to="/auth/signin" replace />} />
+        <Route path="/auth/signup" element={<Signup/>} />
+        <Route path="/auth/signin" element={<Signin/>} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/api/apiConfig.js
@@ -0,0 +1,15 @@
+import axios from 'axios'
+const LOCALSTORAGE_KEY='token'
+const api = axios.create({
+//   baseURL: 'https://luke-used-cars-backend-19ea42e37e12.herokuapp.com/',
+  baseURL: 'http://localhost:8000',
+
+})
+api.interceptors.request.use(config => {
+    const token = localStorage.getItem(LOCALSTORAGE_KEY)
+    config.headers.Authorization = token
+    return config
+})
+
+export default api
+
