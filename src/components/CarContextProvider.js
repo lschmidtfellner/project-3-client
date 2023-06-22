81eeaf2c96e144c66061c502fc6aa9dab5e01143
granth2023 <grantharris38@gmail.com>
2023-06-22 17:56:06 -0400

    homepage no longer produces object from featured cars

--- src/components/CarContextProvider.js
+++ src/components/CarContextProvider.js
@@ -1,25 +1,29 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import axios from 'axios'
+import { AuthContext } from '../context/AuthContextComponent'
 
 export const CarContext = React.createContext()
 
 export const CarContextProvider = (props) => {
+  const { isLoggedIn } = useContext(AuthContext)
   const [cars, setCars] = useState([])
 
   useEffect(() => {
-    axios.get('https://luke-used-cars-backend-19ea42e37e12.herokuapp.com/api/saleposts', 
-    {
-      auth: {
-        username: 'bret',
-        password: 'bret'
-      }
-    })
-      .then(response => {
-        console.log('Response from server:', response.data); // Console log to check server response
-        setCars(response.data);
+    if (isLoggedIn) {
+      axios.get('https://luke-used-cars-backend-19ea42e37e12.herokuapp.com/api/saleposts', 
+      {
+        auth: {
+          username: 'bret',
+          password: 'bret'
+        }
       })
-      .catch((error) => console.log(error))
-  }, [])
+        .then(response => {
+          console.log('Response from server:', response.data); // Console log to check server response
+          setCars(response.data);
+        })
+        .catch((error) => console.log(error))
+    }
+  }, [isLoggedIn])
 
   return (
     <CarContext.Provider value={{ cars }}>{props.children}</CarContext.Provider>
