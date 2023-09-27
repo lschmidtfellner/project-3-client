927c42f3d0d6b544815e38066653256c6be51851
lschmidtfellner <127900788+lschmidtfellner@users.noreply.github.com>
2023-09-27 13:29:29 -0400

    scroll animation working

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import revLogo from '../assets/revlogo.png'
 import { ReactComponent as Hamburger } from '../assets/revmenu.svg'
 import { ReactComponent as CloseX } from '../assets/revx.svg'
@@ -6,7 +6,10 @@ import { useNavigate } from 'react-router-dom'
 
 function Nav({ isLoggedin, setIsLoggedIn }) {
   const [isMenuOpen, setIsMenuOpen] = useState(false)
+  const [isNavVisible, setIsNavVisible] = useState(true);
+  const [lastScrollPosition, setLastScrollPosition] = useState(0);
   const navigate = useNavigate()
+  const loggedIn = localStorage.getItem('loggedIn')
   const menuToggle = () => {
     setIsMenuOpen((prev) => !prev)
   }
@@ -15,17 +18,45 @@ function Nav({ isLoggedin, setIsLoggedIn }) {
     menuToggle()
   }
 
+  const handleScroll = () => {
+    const currentScrollPosition = window.scrollY;
+    if (currentScrollPosition < lastScrollPosition) {
+
+      setIsNavVisible(true);
+    } else {
+      setIsNavVisible(false);
+    }
+    setLastScrollPosition(currentScrollPosition);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [lastScrollPosition]);
+  
+
+  const handleSignout = () => {
+    setIsLoggedIn(false);
+    localStorage.removeItem('user')
+    localStorage.removeItem('token')
+    localStorage.removeItem('loggedIn')
+    navigate('/auth/signin')
+    menuToggle()
+  }
+
   return (
     <div>
       <div onClick={() => menuToggle()} style={{display: isMenuOpen ? 'block' : 'none'}} className='overlay bg-[#000000] fixed opacity-50 top-0 right-0 bottom-0 left-0'>
       </div>
-      <div style={{display: isMenuOpen ? 'block' : 'none'}} className='bg-off-white fixed top-[90px] py-10 leading-10 right-0 left-0 text-center text-2xl border-b border-black'>
+      <div style={{display: isMenuOpen ? 'block' : 'none'}} className='menu bg-off-white fixed top-[90px] py-10 leading-10 right-0 left-0 text-center text-2xl border-b border-black'>
           <h2 onClick={() => handleNavigate('/')}>Home</h2>
           <h2 onClick={() => handleNavigate('/usercarlistings')}>Your Listings</h2>
           <h2 onClick={() => handleNavigate('/post')}>Create Listing</h2>
-          {isLoggedin ? <h2>Sign Out</h2> : <h2>Sign In</h2>}
+          {loggedIn ? <h2 onClick={() => handleSignout()} >Sign Out</h2> : <h2 onClick={() => handleNavigate('/auth/signin')}>Sign In</h2>}
         </div>
-      <div className="fixed bg-off-white border-b border-black flex items-center nav h-[90px] w-full">
+      <div style={{ top: isNavVisible ? '0' : '-100px', transition: 'top 0.3s' }} className="fixed bg-off-white border-b border-black flex items-center nav h-[90px] w-full">
         <div className="flex justify-between w-full px-5">
           <div className="w-6" />
           <img src={revLogo} alt="Rev Logo" className="h-7 w-auto" />
