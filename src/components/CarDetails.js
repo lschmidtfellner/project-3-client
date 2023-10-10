becdf0203fe6562db2fda96277c65ab48e9b0c3a
brooksienyc <cicelybrooks@gmail.com>
2023-10-10 12:27:14 -0400

    2nd commit for CarDetails - tested and working - now shows gmail prompt prepopulated

--- src/components/CarDetails.js
+++ src/components/CarDetails.js
@@ -5,13 +5,22 @@ import { AuthContext } from '../context/AuthContextComponent'; // Import AuthCon
 import { useNavigate } from 'react-router-dom';
 import Swal from 'sweetalert2';
 import api from '../api/apiConfig'; // Import api object
+import ContactSellerBtn from './ContactSellerBtn';
+
+const formatPrice = (price) => {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0
+  }).format(price);
+};
 
 const CarDetails = () => {
   const { cars } = useContext(CarContext);
   const { user } = useContext(AuthContext);
   const [selectedCar, setSelectedCar] = useState(null);
-  const [sellerEmailAddress, setSellerEmailAddress] = useState(null); // Store the seller's email
-  const navigate = useNavigate();
+
 
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
@@ -21,55 +30,8 @@ const CarDetails = () => {
     if (selectedCarId) {
       const car = cars.find((car) => car._id === selectedCarId);
       setSelectedCar(car);
-
-      // Fetch the seller's email based on the user ID in selectedCar
-      const userId = car.user;
-      console.log(userId);
-      fetchSellerEmail(userId);
     }
   }, [cars, selectedCarId]);
-
-  const fetchSellerEmail = async (userId) => {
-    try {
-      // Replace with your actual API endpoint to fetch user data
-      const response = await api.get(`/auth/users/${userId}/email`);
-      console.log(response);
-      const userData = response.data;
-      console.log(userData);
-
-      if (userData.email) {
-        setSellerEmailAddress(userData.email);
-      } else {
-        console.error('Seller email is not available');
-      }
-    } catch (error) {
-      console.error('Error fetching seller email:', error);
-    }
-  };
-
-  const generateEmailAddress = () => {
-    if (user && user.email && sellerEmailAddress) {
-      const subject = encodeURIComponent('Interested in your car listing');
-      const body = encodeURIComponent('Hello, I am interested in your car listing. Please provide me with more details.');
-
-      const mailtoLink = `mailto:${sellerEmailAddress}?subject=${subject}&body=${body}`;
-
-      window.location.href = mailtoLink;
-
-      setTimeout(() => {
-        Swal.fire({
-          icon: 'success',
-          title: `You have successfully contacted ${sellerEmailAddress}`,
-          didClose: () => {
-            navigate(-1); // Redirect back to the previous page
-          },
-        });
-      }, 500);
-    } else {
-      // Handle the case where user.email or sellerEmailAddress is not available
-      console.error('User email or seller email is not available');
-    }
-  };
 
   if (!selectedCar) {
     return <span>Loading...</span>;
@@ -83,16 +45,20 @@ const CarDetails = () => {
       <div key={selectedCar._id}>
         <div className="my-20 text-left border-b px-4">
           <div className="rounded overflow-hidden shadow-lg">
-            <img src={selectedCar.image} alt='Car' className="pb-8 mx-auto"/>
+            <img src={selectedCar.image} alt='Car' className="pb-8 mx-auto" />
           </div>
           <div className="ml-3 mt-8">
             <h2 className="text-lg blue uppercase">{selectedCar.Year} {selectedCar.Make} {selectedCar.Model}</h2>
             <p>mileage: {selectedCar.Mileage}</p>
             <p>condition: {selectedCar.Condition}</p>
             <p>description: {selectedCar.Description}</p>
-            <p>price: {selectedCar.Price}</p>
+            <p>price: {formatPrice(selectedCar.Price)}</p>
           </div>
-          <button onClick={generateEmailAddress} className="rounded-full pink-bg lg:w-1/6 md:w-1/6 py-1 w-1/3  text-white font-bold  hover:text-black mt-4">Contact Seller</button>
+          <ContactSellerBtn 
+            userId={selectedCar.user} 
+            className="rounded-full pink-bg lg:w-1/6 md:w-1/6 py-1 w-1/3  text-white font-bold  hover:text-black mt-4">
+            Contact Seller
+          </ContactSellerBtn>
         </div>
       </div>
     </div>
