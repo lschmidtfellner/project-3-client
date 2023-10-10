7f2312a6ab319dd23b6fec960db0ada4477fed33
brooksienyc <cicelybrooks@gmail.com>
2023-10-10 16:40:46 -0400

    3rd commit - testested locally re CarDetails.

--- src/components/CarDetails.js
+++ src/components/CarDetails.js
@@ -2,24 +2,15 @@ import React, { useContext, useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import { AuthContext } from '../context/AuthContextComponent'; // Import AuthContext
 import { useNavigate } from 'react-router-dom';
-import Swal from 'sweetalert2';
-import api from '../api/apiConfig'; // Import api object
+// import api from '../api/apiConfig'; // Import api object
 import ContactSellerBtn from './ContactSellerBtn';
-
-const formatPrice = (price) => {
-  return new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-    minimumFractionDigits: 0,
-    maximumFractionDigits: 0
-  }).format(price);
-};
 import CarDetailsInfo from './CarDetailsInfo';
 import { getCarsFromSalePost } from '../controller/controller';
 
+
 const CarDetails = () => {
   let { user } = useContext(AuthContext);
-  if (JSON.stringify(user) === '{}') user = JSON.parse(localStorage.getItem('user'));
+  // if (JSON.stringify(user) === '{}') user = JSON.parse(localStorage.getItem('user'));
 
   const [cars, setCars] = useState([])
   const [selectedCar, setSelectedCar] = useState(null);
@@ -36,6 +27,7 @@ const CarDetails = () => {
   useEffect(() => {
     if (selectedCarId && cars.length > 0) {
       const car = cars.find((car) => car._id === selectedCarId);
+      console.log (car._id)
       setSelectedCar(car);
     }
   }, [cars, selectedCarId]);
@@ -48,10 +40,10 @@ const CarDetails = () => {
     <div className='w-100 h-screen bg-off-white'>
       <CarDetailsInfo selectedCar={selectedCar}/>
       <ContactSellerBtn 
-            userId={selectedCar.user} 
-            className="rounded-full pink-bg lg:w-1/6 md:w-1/6 py-1 w-1/3  text-white font-bold  hover:text-black mt-4">
-            Contact Seller
-          </ContactSellerBtn>
+            userId={selectedCar.user}
+            selectedCar={selectedCar}
+            >Contact Seller
+      </ContactSellerBtn>
     </div>
   );
 };
--- src/components/ContactSellerBtn.js
+++ src/components/ContactSellerBtn.js
@@ -2,58 +2,77 @@ import React, { useState, useEffect, useContext } from 'react';
 import Swal from 'sweetalert2';
 import api from '../api/apiConfig';
 import { AuthContext } from '../context/AuthContextComponent';
+import { getCarsFromSalePost } from '../controller/controller';
 
 
-const ContactSellerBtn = ({ userId }) => {
+const ContactSellerBtn = ({ userId, selectedCar }) => {
     const { user } = useContext(AuthContext);
     const [sellerEmailAddress, setSellerEmailAddress] = useState(null);
-  
+
     useEffect(() => {
-      const fetchSellerEmail = async (userId) => {
-        try {
-          const response = await api.get(`/auth/users/${userId}/email`);
-          const userData = response.data;
-  
-          if (userData.email) {
-            setSellerEmailAddress(userData.email);
-          } else {
-            console.error('Seller email is not available');
-          }
-        } catch (error) {
-          console.error('Error fetching seller email:', error);
-        }
-      };
-  
-      fetchSellerEmail(userId);
+        const fetchSellerEmail = async (userId) => {
+            try {
+                const response = await api.get(`/auth/users/${userId}/email`);
+                const userData = response.data;
+
+                if (userData.email) {
+                    setSellerEmailAddress(userData.email);
+                } else {
+                    console.error('Seller email is not available');
+                }
+            } catch (error) {
+                console.error('Error fetching seller email:', error);
+            }
+        };
+
+        fetchSellerEmail(userId);
     }, [userId]);
 
-  const generateEmailAddress = () => {
-    if (user && user.email && sellerEmailAddress) {
-      // Construct the email content for Gmail
-      const subject = encodeURIComponent("I'm interested in your car");
-      const body = encodeURIComponent(
-        'Hello, I am interested in your car listing. Please provide me with more details.'
-      );
-      const gmailLink = `https://mail.google.com/mail/u/0/?tf=cm&to=${sellerEmailAddress}&su=${subject}&body=${body}`;
-  
-      Swal.fire({
-        title: 'Contact Seller',
-        text: 'Click OK to open your Gmail account in a new tab.',
-        icon: 'info',
-        confirmButtonText: 'OK',
-      }).then((result) => {
-        if (result.isConfirmed) {
-          window.open(gmailLink, '_blank');
-        }
-      });
-    };
-  };
+    const generateEmailAddress = () => {
+        if (user && user.email && sellerEmailAddress) {
+            // Construct the email content for Gmail
+            const subject = encodeURIComponent("I'm interested in your car");
+            const formatPrice = (price) => {
+                return new Intl.NumberFormat('en-US', {
+                  style: 'currency',
+                  currency: 'USD',
+                  minimumFractionDigits: 0,
+                  maximumFractionDigits: 0
+                }).format(price);
+              };
+            const carDetails = `
+                Make: ${selectedCar.Make}
+                Model: ${selectedCar.Model}
+                Year: ${selectedCar.Year}
+                Mileage: ${selectedCar.Mileage}
+                Condition: ${selectedCar.Condition}
+                Price: ${formatPrice(selectedCar.Price)}
+                Image: [Click here to view the car image] (${selectedCar.image})
+            `;
 
-  return (
-    <button onClick={generateEmailAddress} className="rounded-full pink-bg lg:w-1/6 md:w-1/6 py-1 w-1/3 text-white font-bold hover:text-black mt-4">
-      Contact Seller
-    </button>
-  );
+            const body = encodeURIComponent(
+                `Hello, I am interested in your car listing. Here are the details of the car I'm interested in:\n\n${carDetails}\n\nPlease provide me with more details.`
+            );
+            const gmailLink = `https://mail.google.com/mail/u/0/?tf=cm&to=${sellerEmailAddress}&su=${subject}&body=${body}`;
+
+            Swal.fire({
+                title: 'Contact Seller',
+                text: 'Click OK to open your Gmail account in a new tab.',
+                icon: 'info',
+                confirmButtonText: 'OK',
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    window.open(gmailLink, '_blank');
+                }
+            });
+        };
+    };
+
+    return (
+        <button onClick={generateEmailAddress} className="rounded-full pink-bg lg:w-1/6 md:w-1/6 py-1 w-1/3 text-white font-bold hover:text-black mt-4">
+            Contact Seller
+        </button>
+    );
 };
 
 export default ContactSellerBtn;
